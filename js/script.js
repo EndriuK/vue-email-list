6921c3ab9be3e3b3c3d6a03f1908b7f3c5c33059
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-07-25 17:17:32 +0200

    [UPD] genero 10 mail randomiche

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+// const { createApp } = Vue;
+
+// createApp({
+//   data() {
+//     return {
+//       email: null,
+//       emails: [],
+//     };
+//   },
+//   methods: {
+//     generateRandomEmail() {
+//       axios
+//         .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//         .then((response) => {
+//           this.email = response.data.response;
+//         });
+//     },
+
+//     generateTenRandomEmails() {
+//       for (let i = 0; i < 10; i++) {
+//         axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+//             this.emails.push(resp.data.response);
+//         });
+
+//     },
+//   },
+// }).mount("#app");
+
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      email: "",
+      email: null,
+      emails: [],
     };
   },
   methods: {
@@ -13,6 +42,17 @@ createApp({
         .then((response) => {
           this.email = response.data.response;
         });
+    },
+
+    generateTenRandomEmails() {
+      this.emails = [];
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((resp) => {
+            this.emails.push(resp.data.response);
+          });
+      }
     },
   },
 }).mount("#app");
