f5f20275c6abdc402075f6f54fed06a7004b5919
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-07-25 16:42:43 +0200

    [UPD] metohd in js e aggiunte in html

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ createApp({
       email: "",
     };
   },
+  methods: {
+    generateRandomEmail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.email = response.data.response;
+        });
+    },
+  },
 }).mount("#app");
